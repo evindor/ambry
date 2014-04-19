1f6834df37dd624a2dcbb787e8f37e1a2e6effe7
Arseny Zarechnev <cyanidesign@gmail.com>
2014-04-20 01:59:53 +0400

    base structure

---
+++ client/views/stars/list.js
@@ -0,0 +1,9 @@
+var starsData = [
+    {url: "http://example.com", name: "Star 1", language: "Javascript"},
+    {url: "http://example2.com", name: "Star 2", language: "Javascript"},
+    {url: "http://example3.com", name: "Star 3", language: "Clojure"}
+];
+
+Template.starsList.helpers({
+    stars: starsData
+});
